83111de11d17e98c9dceee3a35a23d43b323d98c
Chizuru <chizuru.yamauchi@dci-student.org>
2024-03-21 17:40:46 +0100

    add the rest of the test method in UserDaoImplTest class

--- main/test/dao/UserDaoImplTest.java
+++ main/test/dao/UserDaoImplTest.java
@@ -6,10 +6,15 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserDaoImplTest {
     private UserDao userDao;
+
+
+    // all the test has passed successfully
 
     /*
     @Test
@@ -45,5 +50,34 @@ class UserDaoImplTest {
         assertEquals("giyu@hashira_training.com", user.getEmail());
         assertEquals("\\x6c1b87a4b96cd134822a1aeb8e919cbc5992c76c5d2d4d7c6f6784fa1710ef5f", user.getPassword());
     }
+
+    @Test
+    void testReadAllUsers() {
+        userDao = new UserDaoImpl();
+        Set<User> users = userDao.readAllUsers();
+        assertNotNull(users);
+    }
+
+    @Test
+    void testUpdateUser() {
+        userDao = new UserDaoImpl();
+        User userToUpdate = userDao.readUserById(9);
+        userToUpdate.setUsername("updatedUsername");
+        userToUpdate.setEmail("updated@test.com");
+        userToUpdate.setPassword("updatedPassword");
+
+        boolean updated = userDao.updateUser(userToUpdate);
+        assertTrue(updated);
+
+        User updatedUser = userDao.readUserById(9);
+        assertNotNull(updatedUser);
+        assertEquals(userToUpdate.getUsername(), updatedUser.getUsername());
+        assertEquals(userToUpdate.getEmail(), updatedUser.getEmail());
+        assertEquals(userToUpdate.getPassword(), updatedUser.getPassword());
+    }
+
+
+
+
 
 }
